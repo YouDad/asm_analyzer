c726e065b9556016ea0c86931f6a20e1180c6ffd
YouDad <1632083718@qq.com>
2020-10-16 18:00:43 +0800

    ADD: analyzer::get_called_func_by_address

---
+++ libs/analyzer.c
@@ -0,0 +1,37 @@
+#include "common.h"
+#include "analyzer.h"
+
+int get_called_func_by_address(uint32_t address,
+		struct list_head *called_address_list)
+{
+	LIST_HEAD(iblist);
+
+	int ret = get_function_by_address(address, &iblist);
+	if (ret) {
+		return ret;
+	}
+
+	struct instruction_block *item, *tmp;
+	list_for_each_entry_safe(item, tmp, &iblist, list) {
+		struct instruction *i = item->ip;
+
+		while (1) {
+			if (strstr(i->string, "bl") == i->string) {
+				// bl <addr>
+				uint32_t new_address;
+				int ret = sscanf(i->string, "%*s%x", &new_address);
+				if (ret != 1) {
+					return -EINTERNAL;
+				}
+				uint32_list_insert_tail(called_address_list, new_address);
+			}
+			if (i->address < item->end_address) {
+				i++;
+			} else {
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ test/analyzer/get_called_func_by_address.c
@@ -0,0 +1,70 @@
+#include "analyzer.h"
+
+int test(int addr, int *a, int alen);
+int main()
+{
+	int ret = decoder_load("hhee.s");
+	if (ret) {
+		perror("load file hhee.s");
+		return ret;
+	}
+
+	int a1[] = {0x90, 0xa608, 0x2f78};
+	ret = test(0, a1, sizeof(a1));
+	if (ret) {
+		printf("test a1 failed(%d)\n", ret);
+		return 1;
+	}
+
+	int a2[] = {0xa578, 0x90};
+	ret = test(0x110, a2, sizeof(a2));
+	if (ret) {
+		printf("test a2 failed(%d)\n", ret);
+		return 2;
+	}
+
+	int a3[] = {0x872c, 0xa470, 0xde0, 0x872c, 0xa470, 0xde0, 0x8880,
+		0xa4e0, 0x8880, 0xa4e0, 0x47c, 0xde0, 0xde0, 0xbaf8, 0x47c,
+		0x113c, 0x25f4, 0xda8, 0xf24, 0xdb4, 0x872c, 0xa470, 0xa470,
+		0x7cb4, 0xccc, 0x10a0, 0xccc, 0xde0, 0xd54, 0xd54, 0x7cb4,
+		0x79d8, 0xd54, 0xd54, 0xd54, 0xd54, 0xd54, 0xd54, 0xd54, 0xd54,
+		0xd54, 0xd54, 0xd54, 0xd54, 0x7fcc, 0xb74, 0x7c78, 0xccc, 0x10a0,
+		0xde0, 0xb74, 0xb74, 0xd54, 0x7c78, 0xd54, 0xd54, 0xd54};
+	ret = test(0x2e0, a3, sizeof(a3));
+	if (ret) {
+		printf("test a3 failed(%d)\n", ret);
+		return 3;
+	}
+
+	analyzer_clean();
+	return 0;
+}
+
+int test(int addr, int *a, int alen)
+{
+	LIST_HEAD(called_addresses);
+
+	int ret = get_called_func_by_address(addr, &called_addresses);
+	if (ret) {
+		printf("get_called_func_by_address failed(%d)\n", ret);
+		return 1;
+	}
+
+	int i = 0;
+	while (!list_empty(&called_addresses)) {
+		if (i >= alen) {
+			printf("alen assert failed(%d)\n", alen);
+			return 2;
+		}
+
+		int addr = uint32_list_pop_head(&called_addresses);
+		if (a[i] != addr) {
+			printf("address assert failed(%x, %x)\n", a[i], addr);
+			return 3;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
