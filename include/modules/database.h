295633adb00a7bccc3b89db8e4393e895c6c6a9f
YouDad <1632083718@qq.com>
2020-10-22 14:03:02 +0800

    TMP: database

---
+++ include/modules/database.h
@@ -0,0 +1,5 @@
+#include "types.h"
+
+int set_function_name();
+int database_snapshot();
+int database_recover();
---
+++ src/modules/database.c
@@ -0,0 +1,18 @@
+#include "modules/database.h"
+
+int set_function_name()
+{
+	return 0;
+}
+
+int database_snapshot()
+{
+	/* 把内存中的变量存储到快照中 */
+	return 0;
+}
+
+int database_recover()
+{
+	/* 恢复快照 */
+	return 0;
+}
