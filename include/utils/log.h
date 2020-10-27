665bb41a092d2bf3a6e49f3fca670d6f178570c7
YouDad <1632083718@qq.com>
2020-10-27 11:10:00 +0800

    ADD: log

--- include/utils/log.h
+++ include/utils/log.h
@@ -2,6 +2,7 @@
 #include "utils/color.h"
 #include <stdlib.h>
 
+#define log()                printf(_green("(%s: %d)") "\n", __FILE__, __LINE__)
 #define info(fmt, ...)       printf("(%s: %d)" _cyan(fmt) "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define warn(fmt, ...)       printf("(%s: %d)" _yellow(fmt) "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define error(ret, fmt, ...) printf("(%s: %d)" _red(fmt)  "\n", __FILE__, __LINE__, ##__VA_ARGS__); return (ret)
